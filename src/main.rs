d98877628e092c043634ffb6a350df07738e92f0
Aleksandar Dimitrov <git@aleks.bg>
2023-01-16 22:37:50 +0100

    Prepare API surface for WASM

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
 mod maze;
 
+use maze::generator::*;
+use maze::paint::*;
+use maze::*;
+
+fn make_svg_maze(
+    x_size: usize,
+    y_size: usize,
+    seed: u64,
+) -> Result<String, Box<dyn std::error::Error>> {
+    let maze: Maze = jarník(x_size, y_size, seed);
+    let mut str = String::new();
+    PlottersSvgStringWriter::new(&mut str, 40, 4).write_maze(&maze)?;
+    Ok(str)
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     use maze::generator::*;
     use maze::paint::*;
@@ -12,9 +27,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     PlottersBitmapWriter::new("./test.png".into(), 40, 4).write_maze(&maze)?;
     PlottersSvgFileWriter::new("./test.svg".into(), 40, 4).write_maze(&maze)?;
 
-    let mut str = String::new();
-    PlottersSvgStringWriter::new(&mut str, 40, 4).write_maze(&maze)?;
-    println!("{str}");
+    let seed: u64 = rand::prelude::random();
+    let s = make_svg_maze(x_size, y_size, seed)?;
+    println!("{seed}\n{s}");
 
     Ok(())
 }
