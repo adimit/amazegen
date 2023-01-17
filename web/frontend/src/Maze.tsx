a584a43aea71e87ce04c4748312575467392394a
Aleksandar Dimitrov <git@aleks.bg>
2023-01-17 23:39:55 +0100

    Store seed in hash

--- web/frontend/src/Maze.tsx
+++ web/frontend/src/Maze.tsx
@@ -1,16 +1,45 @@
 import { make_svg_maze, generate_seed } from "./pkg/maze";
-import { JSX, createSignal, createEffect } from "solid-js";
+import { JSX, createSignal, createEffect, Accessor } from "solid-js";
+
+const getPreseedFromHash = (): bigint => {
+  if (document?.location.hash === "") {
+    return generate_seed();
+  }
+  try {
+    return BigInt(document?.location.hash.substring(1));
+  } catch (e) {
+    console.log("defaulting to random");
+    return generate_seed();
+  }
+};
+
+const seedSignal = (): {
+  seed: Accessor<bigint>;
+  regenerateSeed: () => void;
+} => {
+  const [seed, setSeed] = createSignal(getPreseedFromHash());
+  createEffect(() => {
+    if (document?.location) {
+      document.location.hash = seed().toString();
+    }
+  });
+
+  return { seed, regenerateSeed: () => setSeed(generate_seed()) };
+};
 
 export default function Maze(): JSX.Element {
   let svgRef: HTMLDivElement | undefined;
+  let input: HTMLInputElement | undefined;
+
   const [size, setSize] = createSignal(10);
-  const seed = generate_seed();
+  const { seed, regenerateSeed } = seedSignal();
+
   createEffect(() => {
     if (svgRef !== undefined) {
-      svgRef.innerHTML = make_svg_maze(size(), size(), seed);
+      svgRef.innerHTML = make_svg_maze(size(), size(), seed());
     }
   });
-  let input: HTMLInputElement | undefined;
+
   return (
     <>
       <input
@@ -23,6 +52,7 @@ export default function Maze(): JSX.Element {
           }
         }}
       />
+      <button onClick={regenerateSeed}>Refresh</button>
       <div ref={svgRef} />
     </>
   );
