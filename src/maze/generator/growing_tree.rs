5c172f2f2187f936f23e8bba521e6dafb555bd93
Aleksandar Dimitrov <git@aleks.bg>
2023-08-31 00:41:17 +0200

    Add migrated algorithm files

---
+++ src/maze/generator/growing_tree.rs
@@ -0,0 +1,65 @@
+use super::{make_random_longest_exit, MazeGenerator};
+use crate::maze::{
+    regular::RectilinearMaze,
+    theta::{PolarNode, ThetaMaze},
+    Maze, Node,
+};
+
+pub struct GrowingTreeGenerator<C: Node> {
+    extents: C,
+    seed: u64,
+}
+
+impl<C: Node> GrowingTreeGenerator<C> {
+    fn jarník<M: Maze<NodeType = C>>(&self, mut maze: M) -> M {
+        let mut vertices: Vec<C> = vec![];
+        fastrand::seed(self.seed);
+
+        {
+            let start = C::get_random_node(self.extents);
+            maze.visit(start);
+            vertices.push(start);
+        }
+
+        while !vertices.is_empty() {
+            let i = vertices.len() - 1;
+            let e = vertices[i];
+            let targets = maze.get_possible_targets(e);
+            if !targets.is_empty() {
+                let target = targets[fastrand::usize(..targets.len())];
+                assert!(maze.move_from_to(e, target));
+                vertices.push(target);
+            } else {
+                vertices.remove(i);
+            }
+        }
+
+        maze
+    }
+}
+
+impl GrowingTreeGenerator<(usize, usize)> {
+    pub fn new(extents: (usize, usize), seed: u64) -> Self {
+        Self { extents, seed }
+    }
+}
+
+impl GrowingTreeGenerator<PolarNode> {
+    pub fn new(extents: PolarNode, seed: u64) -> Self {
+        Self { extents, seed }
+    }
+}
+
+impl MazeGenerator<RectilinearMaze> for GrowingTreeGenerator<(usize, usize)> {
+    fn generate(&mut self) -> RectilinearMaze {
+        let mut maze = self.jarník(RectilinearMaze::new(self.extents));
+        make_random_longest_exit(&mut maze);
+        maze
+    }
+}
+
+impl MazeGenerator<ThetaMaze> for GrowingTreeGenerator<PolarNode> {
+    fn generate(&mut self) -> ThetaMaze {
+        ThetaMaze::new(10.0, self.extents)
+    }
+}
