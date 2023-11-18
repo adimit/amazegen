a4c2801c0439f547cc7cea66fd0066d682631c25
Aleksandar Dimitrov <git@aleks.bg>
2023-11-18 21:07:37 +0100

    Factor out logical maze, polar computations and drawing code

--- src/maze/polar.rs
+++ src/maze/polar.rs
@@ -6,37 +6,86 @@ use svg::{Document, Node};
 
 const π: f64 = std::f64::consts::PI;
 
+struct RingMaze {
+    rings: Vec<usize>,
+}
+
+impl RingMaze {
+    fn columns(row: usize, column_factor: usize) -> usize {
+        2_usize.pow(row.ilog(2)) * column_factor
+    }
+
+    pub fn new(column_factor: usize, max_rings: usize) -> RingMaze {
+        let mut rings = vec![1];
+        rings.extend(
+            (1..max_rings)
+                .into_iter()
+                .map(|row| Self::columns(row, column_factor)),
+        );
+        RingMaze { rings }
+    }
+
+    /// No bounds checking on `ring`. Panics if `ring` > `rings` of this maze
+    pub fn max_column(&self, ring: usize) -> usize {
+        self.rings[ring]
+    }
+}
+
 #[derive(Copy, Clone, Debug)]
 pub struct PolarNode {
     pub row: usize,
     pub column: usize,
 }
 
-const RING_HEIGHT: f64 = 40.0;
-
-fn max_column(row: usize) -> usize {
-    ((row as f64 / 2.0).ceil() as usize) * 10
+struct PolarGrid<'a> {
+    ring_height: f64,
+    maze: &'a RingMaze,
 }
 
-fn θ(row: usize) -> f64 {
-    // TODO: cache theta values?
-    2.0 * π / max_column(row) as f64
-}
+impl PolarGrid<'_> {
+    fn new(maze: &RingMaze, ring_height: f64) -> PolarGrid {
+        PolarGrid { maze, ring_height }
+    }
 
-fn inner_radius(row: usize) -> f64 {
-    RING_HEIGHT * row as f64
-}
+    fn θ(&self, row: usize) -> f64 {
+        // TODO: cache theta values?
+        2.0 * π / self.maze.max_column(row) as f64
+    }
 
-fn outer_radius(row: usize) -> f64 {
-    RING_HEIGHT * (row + 1) as f64
-}
+    fn inner_radius(&self, row: usize) -> f64 {
+        self.ring_height * row as f64
+    }
 
-fn θ_west(node: PolarNode) -> f64 {
-    θ(node.row) * (1.0 + node.column as f64)
-}
+    fn outer_radius(&self, row: usize) -> f64 {
+        self.ring_height * (row + 1) as f64
+    }
 
-fn θ_east(node: PolarNode) -> f64 {
-    θ(node.row) * (node.column as f64)
+    fn θ_west(&self, node: PolarNode) -> f64 {
+        self.θ(node.row) * (1.0 + node.column as f64)
+    }
+
+    fn θ_east(&self, node: PolarNode) -> f64 {
+        self.θ(node.row) * (node.column as f64)
+    }
+
+    fn compute_cell(&self, node: PolarNode) -> CellCoordinates {
+        let inner = self.inner_radius(node.row);
+        let outer = self.outer_radius(node.row);
+        let east = self.θ_east(node);
+        let west = self.θ_west(node);
+        let centre = CartesianPoint { x: 500, y: 500 };
+
+        CellCoordinates {
+            ax: centre.x as f64 + (inner * west.cos()),
+            ay: centre.y as f64 + (inner * west.sin()),
+            bx: centre.x as f64 + (outer * west.cos()),
+            by: centre.y as f64 + (outer * west.sin()),
+            cx: centre.x as f64 + (inner * east.cos()),
+            cy: centre.y as f64 + (inner * east.sin()),
+            dx: centre.x as f64 + (outer * east.cos()),
+            dy: centre.y as f64 + (outer * east.sin()),
+        }
+    }
 }
 
 #[derive(Copy, Clone, Debug)]
@@ -59,33 +108,16 @@ struct CellCoordinates {
 
 pub fn test_maze() -> Result<(), ()> {
     println!("Generating maze...");
-
-    fn compute_cell(node: PolarNode) -> CellCoordinates {
-        let inner = inner_radius(node.row);
-        let outer = outer_radius(node.row);
-        let east = θ_east(node);
-        let west = θ_west(node);
-        let centre = CartesianPoint { x: 500, y: 500 };
-
-        CellCoordinates {
-            ax: centre.x as f64 + (inner * west.cos()),
-            ay: centre.y as f64 + (inner * west.sin()),
-            bx: centre.x as f64 + (outer * west.cos()),
-            by: centre.y as f64 + (outer * west.sin()),
-            cx: centre.x as f64 + (inner * east.cos()),
-            cy: centre.y as f64 + (inner * east.sin()),
-            dx: centre.x as f64 + (outer * east.cos()),
-            dy: centre.y as f64 + (outer * east.sin()),
-        }
-    }
+    let maze = RingMaze::new(8, 10);
+    let grid = PolarGrid::new(&maze, 40.0);
 
     let mut document = Document::new().set("viewBox", (0, 0, 1000, 1000));
 
-    fn arc(column: usize, row: usize) -> Path {
+    fn arc(grid: &PolarGrid, column: usize, row: usize) -> Path {
         let node = PolarNode { column, row };
-        let cell = compute_cell(node);
-        let outer = outer_radius(node.row);
-        let inner = inner_radius(node.row);
+        let cell = grid.compute_cell(node);
+        let outer = grid.outer_radius(node.row);
+        let inner = grid.inner_radius(node.row);
         let data = Data::new()
             .move_to((cell.ax, cell.ay))
             .line_to((cell.bx, cell.by))
@@ -106,9 +138,9 @@ pub fn test_maze() -> Result<(), ()> {
             .set("stroke-width", "3")
     }
 
-    for row in 1..10 {
-        for column in 0..max_column(row) {
-            document.append(arc(column, row));
+    for row in 1..maze.rings.len() {
+        for column in 0..maze.max_column(row) {
+            document.append(arc(&grid, column, row));
         }
     }
 
