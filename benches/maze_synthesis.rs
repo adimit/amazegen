e9f7c5245b020f73aaec9ffbec66c67460479585
Aleksandar Dimitrov <git@aleks.bg>
2023-11-25 13:13:46 +0100

    Add benchmarks

--- benches/maze_synthesis.rs
+++ benches/maze_synthesis.rs
@@ -1,22 +1,86 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use maze::{
-    config::{Algorithm::*, Configuration, Shape::Rectilinear},
     generate_seed,
+    maze::feature::{Algorithm, Configuration, Shape},
 };
 
-fn criterion_benchmark(c: &mut Criterion) {
+fn kruskal_rect(c: &mut Criterion) {
     c.bench_function("maze_synthesis", |b| {
         b.iter(|| {
             Configuration {
-                algorithm: Kruskal,
+                algorithm: Algorithm::Kruskal,
                 colour: "000000".into(),
                 features: vec![],
                 seed: generate_seed(),
-                shape: Rectilinear(10, 10),
+                shape: Shape::Rectilinear(10, 10),
+                stroke_width: 4.0,
             }
             .execute()
         })
     });
 }
-criterion_group!(benches, criterion_benchmark);
+
+fn jarník_rect(c: &mut Criterion) {
+    c.bench_function("maze_synthesis", |b| {
+        b.iter(|| {
+            Configuration {
+                algorithm: Algorithm::GrowingTree,
+                colour: "000000".into(),
+                features: vec![],
+                seed: generate_seed(),
+                shape: Shape::Rectilinear(10, 10),
+                stroke_width: 4.0,
+            }
+            .execute()
+        })
+    });
+}
+
+fn jarník_theta(c: &mut Criterion) {
+    let mut group = c.benchmark_group("theta");
+    group.warm_up_time(std::time::Duration::from_secs(6));
+    group.measurement_time(std::time::Duration::from_secs(10));
+    group.bench_function("maze_synthesis", |b| {
+        b.iter(|| {
+            Configuration {
+                algorithm: Algorithm::GrowingTree,
+                colour: "000000".into(),
+                features: vec![],
+                seed: generate_seed(),
+                shape: Shape::Theta(10),
+                stroke_width: 4.0,
+            }
+            .execute()
+        })
+    });
+    group.finish();
+}
+
+fn kruskal_theta(c: &mut Criterion) {
+    let mut group = c.benchmark_group("theta");
+    group.warm_up_time(std::time::Duration::from_secs(6));
+    group.measurement_time(std::time::Duration::from_secs(10));
+    group.bench_function("maze_synthesis", |b| {
+        b.iter(|| {
+            Configuration {
+                algorithm: Algorithm::Kruskal,
+                colour: "000000".into(),
+                features: vec![],
+                seed: generate_seed(),
+                shape: Shape::Theta(10),
+                stroke_width: 4.0,
+            }
+            .execute()
+        })
+    });
+    group.finish();
+}
+
+criterion_group!(
+    benches,
+    // kruskal_rect,
+    // jarník_rect,
+    kruskal_theta,
+    jarník_theta
+);
 criterion_main!(benches);
