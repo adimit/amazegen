8d11390139ddc2e4915473818e3a634588045570
Aleksandar Dimitrov <git@aleks.bg>
2025-03-22 22:29:15 +0100

    Fix rendering the wrong seed

--- amazegen_printer/src/lib.rs
+++ amazegen_printer/src/lib.rs
@@ -22,13 +22,13 @@ pub fn generate_pdf(js: JsValue, pages: u32, baseurl: String, font: Vec<u8>) -> 
     let url = Some(baseurl);
     for _ in 0..pages {
         let (maze, new_seed) = configuration.execute_for_svg();
-        configuration.seed = new_seed;
         let metadata = Metadata::from_configuration(&configuration, url.clone());
         let maze_with_metadata = metadata.metadata_to_render(maze, &font_name.clone());
         let svg = Svg {
             content: maze_with_metadata.document.to_string(),
             dimensions: maze_with_metadata.dimensions,
         };
+        configuration.seed = new_seed;
         pdf.append_maze(&svg);
     }
     pdf.write_to_memory()
